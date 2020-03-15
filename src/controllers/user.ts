ad9d0884cebdc44ab906ffdd38914a7fefa9c613
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-03-15 19:09:51 +0700

    Add getUsers controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,9 +2,14 @@ import type { Context } from 'koa'
 import userModel from '../models/user'
 
 export default class UserController {
-  public static async createUser(ctx: Context) {
+  public static async createUser(ctx: Context): Promise<void> {
     const { name } = ctx.request.body
     const result = await userModel.create({ name })
+    ctx.body = result
+  }
+
+  public static async getUsers(ctx: Context): Promise<void> {
+    const result = await userModel.find()
     ctx.body = result
   }
 }
--- src/privateRoutes.ts
+++ src/privateRoutes.ts
@@ -2,6 +2,7 @@ import * as Router from 'koa-router'
 import * as Controllers from './controllers'
 
 const privateRouter: Router = new Router()
+privateRouter.get('/user', Controllers.UserController.getUsers)
 privateRouter.post('/user', Controllers.UserController.createUser)
 
 export { privateRouter }
