724f77fbcf7356b24a9d32b808afaaadc50b3183
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-03-15 20:04:06 +0700

    Add koa-helmet dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import * as Koa from 'koa'
-import * as KoaLogger from 'koa-logger'
+import * as Logger from 'koa-logger'
+import * as BodyParser from 'koa-bodyparser'
+import * as Helmet from 'koa-helmet'
 
 import { dbConnect } from './db'
 import { publicRouter } from './publicRoutes'
 import { privateRouter } from './privateRoutes'
-import * as BodyParser from 'koa-bodyparser'
 
 const app: Koa = new Koa()
 
 dbConnect().then(() => {
-  app.use(KoaLogger())
+  app.use(Logger())
   app.use(BodyParser())
+  app.use(Helmet())
 
   app.use(publicRouter.routes()).use(publicRouter.allowedMethods())
   app.use(privateRouter.routes()).use(privateRouter.allowedMethods())
